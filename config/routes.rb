44d69731893fd3a9ec7250277d4bdbc5a2d5936d
YvesCesar <yvesamorim73@gmail.com>
2022-09-28 16:17:36 -0400

    Fish tasks creation with EDIT and DELETE operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "tasks#index"
 
   resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post "tasks/:id/toggle", to: "tasks#toggle"
 end
